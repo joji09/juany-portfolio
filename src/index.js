392327ff1c86417c6bf27d39be261071830f8a33
juany <juaaany@gmail.com>
2024-05-03 19:48:27 -0400

    updated index.js

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import reportWebVitals from "./reportWebVitals";
 
 const root = document.getElementById('root');
 
@@ -13,9 +14,4 @@ rootElement.render(
   </React.StrictMode>
 );
 
-// ReactDOM.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>,
-//   document.getElementById('root')
-// );
+reportWebVitals();
