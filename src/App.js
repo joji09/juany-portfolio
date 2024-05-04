85473f3495ec3814f171409ddc024f2dc36c4d0e
juany <juaaany@gmail.com>
2024-05-03 20:25:53 -0400

    switched Router for HashRouter

--- src/App.js
+++ src/App.js
@@ -1,20 +1,33 @@
-import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route, HashRouter } from "react-router-dom";
 import Home from "./views/Home";
 import routePaths from "./routes/routePaths";
 import content from "./content";
 import ProjectInfo from "./views/ProjectInfo";
 
 
+// function App() {
+//   return (
+//     <Router>
+//       <Routes>
+//         {content.projects.map((project) => (
+//           <Route key={project.path} path={"/" + project.path} element={<ProjectInfo ProjectInfoObject={project} />} />
+//         ))}
+//         <Route path="/" element={<Home />} />
+//       </Routes>
+//     </Router>
+//   );
+// }
+
 function App() {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         {content.projects.map((project) => (
           <Route key={project.path} path={"/" + project.path} element={<ProjectInfo ProjectInfoObject={project} />} />
         ))}
         <Route path="/" element={<Home />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
